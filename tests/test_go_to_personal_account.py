ec3a2371a0980007362cdec3c4f2b882e041f23f
Anastasiia Zhabchenko <anastasiazh26@gmail.com>
2022-11-16 18:28:05 +0300

    исправила урл

--- tests/test_go_to_personal_account.py
+++ tests/test_go_to_personal_account.py
@@ -1,12 +1,14 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
 
 class TestPersonalAccount:
     def test_login_by_personal_account_button(self, login):
-        #Кнопка Личный Кабинет
+        # Кнопка Личный Кабинет
         login.find_element(By.XPATH, "//p[contains(text(),'Личный Кабинет')]").click()
-        #Используем явное ожидание по url
-        WebDriverWait(login, 3).until(expected_conditions.url_contains("https://stellarburgers.nomoreparties.site/account"))
-        #Сравниваем текущий урл с ожидаемым
-        assert login.current_url == 'https://stellarburgers.nomoreparties.site/account'
+        # Используем явное ожидание по url
+        WebDriverWait(login, 5).until(EC.url_contains("https://stellarburgers.nomoreparties.site"
+                                                      "/account/profile"))
+        # Сравниваем текущий урл с ожидаемым
+        assert login.current_url == 'https://stellarburgers.nomoreparties.site/account/profile'
